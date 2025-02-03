68942295476a9cc7f61b82725eb803b71374a980
KO JIHYE <129650854+jihyee0e@users.noreply.github.com>
2025-02-04 08:35:29 +0900

    Update 연습.py

--- 브루트포스알고리즘/연습.py
+++ 브루트포스알고리즘/연습.py
@@ -2,23 +2,23 @@
 # N개의 자연수가 존재하므로 O(N), 각 수에 대해 소수인지 판별해야 하므로 O(N)
 # => 시간 복잡도는 O(N^2)이 나오게 되고, N은 1000이하의 자연수 이므로 이 풀이는 100만번 연산 내에 문제를 해결할 수 있게 됨.
 
-# n = 1000
-# result = 0
+n = 1000
+result = 0
 
-# for i in range(1, n+1):  # O(N)
-# 	if i == 1:  # 1은 예외처리
-# 		continue
+for i in range(1, n+1):  # O(N)
+	if i == 1:  # 1은 예외처리
+		continue
 	
-# 	is_prime = True  # 소수인지 아닌지
+	is_prime = True  # 소수인지 아닌지
 	
-# 	for j in range(2, i):  # O(i)
-# 		if i % j == 0:  # 1과 i를 제외한 다른 것이 존재
-# 			is_prime = False
+	for j in range(2, i):  # O(i)
+		if i % j == 0:  # 1과 i를 제외한 다른 것이 존재
+			is_prime = False
 		
-# 	if is_prime:  # 소수일 때만 +1
-# 		result += 1
+	if is_prime:  # 소수일 때만 +1
+		result += 1
 
-# print(result)
+print(result)
 # -------------------------------------------------------
 from math import sqrt
 
